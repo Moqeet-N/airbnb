5048f3ca1a28a9de969a4dddb81c8e53018b67c1
MoqeetN <90436920+Moqeet-N@users.noreply.github.com>
2022-02-20 20:36:52 -0500

    Small listings: Layout done need to add details

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Header from '../components/Header';
 import Banner from '../components/Banner'
+import SmallCard from '../components/SmallCard';
 
-export default function Home() {
+export default function Home({exploreData}) {
   return (
     <div className="">
       <Head>
@@ -13,6 +14,32 @@ export default function Home() {
       <Header/>
       <Banner/>
 
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+          <section className='pt-6'>
+            <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+
+            {exploreData?.map(({img, distance, location}) => (
+              <SmallCard
+                key={img} 
+                img={img}
+                distance={distance}
+                location={location}
+                />
+            ))}
+          </section>
+      </main>
+
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch('https://jsonkeeper.com/b/6HPY').
+  then(res => res.json());
+
+  return {
+    props:{
+      exploreData
+    }
+  }
 }
